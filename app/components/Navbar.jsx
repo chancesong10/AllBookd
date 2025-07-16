e3726ec89372c22aeb3d46519f5f52b3ead60e7e
Temirlan Nurmambetov <tnurmamb@students.cs.ubc.ca>
2025-07-16 15:41:36 -0600

    Added a searchbar and kind centered a bit the navbar links

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -10,16 +10,24 @@ export default function Navbar() {
 
     return (
         <nav>
-            <div className="fixed bg-gray-900 p-4 w-full">
-                <ul className="flex space-x-4">
-                    {Links.map((link) => (
-                        <li key={link.href}>
-                            <Link href={link.href} className="text-white px-4 py-2 rounded-md hover:bg-blue-800 transition">
-                                {link.text}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+            <div className="fixed bg-gray-900 p-4 w-full flex items-center">
+                <div className="flex-1 flex justify-center ">
+                    <ul className="flex space-x-4">
+                        {Links.map((link) => (
+                            <li key={link.href}>
+                                <Link href={link.href} className="text-white px-4 py-2 rounded-md hover:bg-blue-800 transition">
+                                    {link.text}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="ml-auto">
+                    <input 
+                    type="text" 
+                    placeholder="Search Books"
+                    className="px-4 py-2 rounded-xl bg-blue-950 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-700"/>
+                </div>
             </div>
         </nav>
     )
