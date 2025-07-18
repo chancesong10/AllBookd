4d6f7247ada0a85c513cc7e9edf27d38f8d648b3
Temirlan Nurmambetov <tnurmamb@students.cs.ubc.ca>
2025-07-18 17:18:11 -0600

    Test Push

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@ export default function Page() {
     <div className="bg-black text-white min-h-screen w-full flex flex-col items-center justify-center">
       <h1 className="text-5xl font-bold">Bookboxd</h1>
       <p className="mt-4 text-lg text-gray-400">Your personal reading log, reimagined.</p>
-    </div>
+    </div> //main page
   );
 }
