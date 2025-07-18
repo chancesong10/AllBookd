a6c295167d11f224887318bfae6d121937fcd1ee
Temirlan Nurmambetov <tnurmamb@students.cs.ubc.ca>
2025-07-18 15:55:26 -0600

    Added search function to the search inpute on navbar.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,12 +1,25 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Navbar() {
+    const [query, setQuery] = useState('')
+    const router = useRouter()
+
     const Links = [
         {href: "/", text: "Home"},
         {href: "/books", text: "Books"},
         {href: "/lists", text: "Lists"},
         {href: "/about", text: "About"},
     ]
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter' && query.trim()) {
+          router.push(`/search?q=${encodeURIComponent(query.trim())}`)
+        }
+    }
 
     return (
         <nav>
@@ -26,6 +39,9 @@ export default function Navbar() {
                     <input 
                     type="text" 
                     placeholder="Search Books"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    onKeyDown={handleKeyDown}
                     className="px-4 py-2 rounded-xl bg-blue-950 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-700"/>
                 </div>
             </div>
