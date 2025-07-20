429632bf98e12c916d653b3df47c9f11b99064e8
Temirlan Nurmambetov <tnurmamb@students.cs.ubc.ca>
2025-07-20 17:38:01 -0600

    Added a second header on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Page() {
   return (
-    <div className="bg-black text-white min-h-screen w-full flex flex-col items-center justify-center">
-      <h1 className="text-5xl font-bold">Bookboxd</h1>
+    <div className="bg-black text-white min-h-screen w-full flex flex-col items-center justify-center text-center">
+      <h1 className="text-5xl font-bold mb-5">Bookboxd</h1>
+      <h2 className="text-2xl font-bold">
+        Keep Track of Books You Love
+        <br></br>
+        Share Your Taste With Your Friends</h2>
       <p className="mt-4 text-lg text-gray-400">Your personal reading log, reimagined.</p>
     </div> //main page
   );
