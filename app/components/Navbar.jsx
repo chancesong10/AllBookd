5f5f858679d6f2f6e14a3539cd38a8d70235dfa3
Temirlan Nurmambetov <cdefrst58@gmail.com>
2025-07-24 15:43:21 -0600

    Added  search icon on the search bar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -40,14 +40,19 @@ export default function Navbar() {
                         ))}
                     </ul>
                 </div>
-                <div className="ml-auto">
+                <div className="ml-auto relative">
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                        <svg className="w-4 h-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
+                    </div>
                     <input 
                     type="text" 
                     placeholder="Search Books"
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
                     onKeyDown={handleKeyDown}
-                    className="px-4 py-2 rounded-xl bg-blue-950 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-700"/>
+                    className="pl-10 pr-4 py-2 rounded-xl bg-blue-950 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-700"/>
                 </div>
             </div>
         </nav>
