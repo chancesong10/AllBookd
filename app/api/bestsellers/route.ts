538b6e271a51982e44be2a9e826167f9ad55df9c
Temirlan Nurmambetov <cdefrst58@gmail.com>
2025-08-09 16:12:51 -0600

    Implemented caching NYT API data results to prevent exceeding quota on calls to the api

--- app/api/bestsellers/route.ts
+++ app/api/bestsellers/route.ts
@@ -1,19 +1,32 @@
 import { NextResponse } from 'next/server';
 
+let cache: Record<string, { data: any; timestamp: number }> = {};
+
 export async function GET(request: Request) {
     const nytKey = process.env.NYT_BOOKS_API_KEY;
     const googleKey = process.env.GOOGLE_BOOKS_API_KEY;
     const { searchParams } = new URL(request.url);
     const category = searchParams.get('category') || 'hardcover-fiction';
 
+    const now = Date.now();
+    const cached = cache[category];
+
+    if (cached && now - cached.timestamp < 24 * 60 * 60 * 1000) { // 24 hours cache
+        console.log(`Serving ${category} from cache`);
+        return NextResponse.json(cached.data);
+    }
+
     // Get NYT bestsellers
     const nytRes = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/${category}.json?api-key=${nytKey}`);
     const nytData = await nytRes.json();
 
-    const books = nytData.results.books || [];
+    if (!nytData?.results?.books) {
+        console.error('NYT API error:', nytData);
+        return NextResponse.json({ results: [], error: 'Failed to fetch NYT bestsellers' }, { status: 500 });
+    }
 
     // ISBN to get google books data
-    const googleRequests = books.map(async (book: any) => {
+    const googleRequests = nytData.results.books.map(async (book: any) => {
         const isbn = book.primary_isbn13 || book.primary_isbn10;
 
         if (!isbn) return null;
@@ -26,8 +39,11 @@ export async function GET(request: Request) {
         };
     });
 
+
+
     const results = (await Promise.all(googleRequests)).filter(Boolean);
+    // Store in category-specific cache
+    cache[category] = { data: { results }, timestamp: now };
 
     return NextResponse.json({ results });
-
 }
