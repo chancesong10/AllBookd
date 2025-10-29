044075f2fb9e1a2ef686f7d7a5e5d25b6a391a56
Chance <chancesong10@gmail.com>
2025-10-28 22:05:58 -0700

    updated about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
     return (
         <div className="bg-black text-white min-h-screen w-full flex flex-col items-center justify-center">
           <h1 className="text-5xl font-bold">About us</h1>
-          <p className="mt-4 text-lg text-gray-400">Two Kids who Want a job</p>
+          <p className="mt-4 text-lg text-gray-400">AllBookd is your personal space to explore, wishlist, and log your reading adventures.</p>
         </div>
     );
 }
