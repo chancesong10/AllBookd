69337712c2af4135eef08390a78962864e5ddb31
Chance <chancesong10@gmail.com>
2025-11-03 12:20:40 -0800

    added speed checks

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import Navbar from '@/app/components/Navbar'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
           <Navbar />
           <main>{children}</main>
           <Analytics/>
+          <SpeedInsights/>
       </body>
     </html>
   )
